3f13c775e435c299ff392894b09f2082fed2be2a
lynx7843 <lynx47547@gmail.com>
2026-09-03 23:03:35 +0530

    education section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import TechStack from "@/components/TechStack";
 import Projects from "@/components/Projects";
+import Education from "@/components/Education";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Hero />
         <TechStack />
         <Projects />
+        <Education />
       </main>
     </>
   );
